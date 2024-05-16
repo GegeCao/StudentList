81ffaf13ceb94e60cddb27dfc4002d87a1eafa02
GegeCao <caoge@sheridancollege.ca>
2024-05-16 11:53:32 -0400

    Revert "Second comment"
    This reverts commit b666bb72854b56ad53b233b875d259ac0d0bf4c2.

--- src/studentlist/StudentList.java
+++ src/studentlist/StudentList.java
@@ -16,10 +16,6 @@ public class StudentList {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("Hello World");
-        System.out.println("Hello World 2");
-     
-        
-        
     }
     
 }
